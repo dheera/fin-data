4933d99915dc4b60cb7c9fba205b6b1ce09f0448
dheera <dheera@dheera.net>
2025-03-05 07:15:57 +0000

    only update most recent

--- download_splits.py
+++ download_splits.py
@@ -5,13 +5,17 @@ import pandas as pd
 import json
 import requests
 import glob
+from tqdm import tqdm
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 
 with open("polygon.json", "r") as f:
     config = json.loads(f.read())
 
 s = requests.Session()
 
-def get_splits():
+def get_splits(start=None):
     url = "https://api.polygon.io/v3/reference/splits"
     results = []
     while url is not None:
@@ -20,39 +24,48 @@ def get_splits():
             params = {
                 "apiKey": config["api_key"],
                 "market": "stocks",
+                "execution_date.gt": start,
                 "limit": 1000,
             }
         )
         response = r.json()
         results += response["results"]
         url = response.get("next_url", None)
-        print(response)
-        print(len(results))
+        print(f"Fetched {len(results)} splits")
         time.sleep(0.1)
     return results
 
 if __name__ == "__main__":
-    # download splits 1 ticker at a time, polygon doesn't have an API to get all of them
-    #os.makedirs("us_stocks_sip/splits_by_ticker", exist_ok = True)
-    #tickers = pd.read_parquet("us_stocks_sip/tickers.parquet")["ticker"].to_list()
-    #for ticker in tickers:
-    #    get_splits(ticker)
-    #time.sleep(0.015)
+    file_path = "us_stocks_sip/splits.parquet"
+    if os.path.exists(file_path):
+        timestamp = os.path.getmtime(file_path)
+        last_modified = datetime.fromtimestamp(timestamp)
+        six_months_ago = last_modified - relativedelta(months=6)
 
-    # Step 1: Read all parquet files from the splits directory we just downloaded
-    #files = glob.glob("us_stocks_sip/splits_by_ticker/*.parquet")
-    #dfs = [pd.read_parquet(file) for file in files]
-    #df = pd.concat(dfs, ignore_index=True)
+        print(f"Fetching splits from {six_months_ago.strftime('%Y-%m-%d')}")
+        splits = get_splits(start = six_months_ago.strftime("%Y-%m-%d"))
 
-    splits = get_splits()
-    df = pd.DataFrame(splits)
+        df_old = pd.read_parquet(file_path)
+        df = pd.DataFrame(splits)
+        df["execution_date"] = pd.to_datetime(df["execution_date"], errors="coerce").dt.date
+        df.drop(columns=["id"], inplace=True)
+        df.set_index(["ticker", "execution_date"], inplace=True)
 
-    df["execution_date"] = pd.to_datetime(df["execution_date"], errors="coerce").dt.date
-    df.drop(columns=["id"], inplace=True)
-    df.set_index(["ticker", "execution_date"], inplace=True)
-    df.sort_index(inplace=True)
+        df = pd.concat([df_old, df])
+        df = df[~df.index.duplicated(keep='first')]
+        df.sort_index(inplace=True)
 
-    print(df)
+    else:
+        print(f"Fetching all splits")
+        splits = get_splits()
+        df = pd.DataFrame(splits)
+        df["execution_date"] = pd.to_datetime(df["execution_date"], errors="coerce").dt.date
+        df.drop(columns=["id"], inplace=True)
+        df.set_index(["ticker", "execution_date"], inplace=True)
 
+        df.sort_index(inplace=True)
+
+    print("Writing us_stocks_sip/splits.parquet")
     df.to_parquet("us_stocks_sip/splits.parquet")
+
 
